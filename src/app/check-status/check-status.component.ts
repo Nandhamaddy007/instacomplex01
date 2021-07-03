cc77c88d3d1ecc5b08d718f91426ac5483791846
Nandhamaddy007 <nandhamaddy007@gmail.com>
2021-07-03 18:13:42 +0530

    2 files modified

--- src/app/check-status/check-status.component.ts
+++ src/app/check-status/check-status.component.ts
@@ -9,12 +9,14 @@ import { BackendTalkerService } from '../backend-talker.service';
 export class CheckStatusComponent implements OnInit {
   constructor(private service: BackendTalkerService) {}
   orderId;
+  orderStatus: any;
   ngOnInit() {
     console.log(String(new Date().getFullYear()));
   }
   getOrderStatus() {
     //INC0307O2
     this.service.getOrderStatus(this.orderId).subscribe(res => {
+      this.orderStatus = res;      
       console.log(res);
     });
   }
